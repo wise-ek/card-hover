0898110dde194b2c393dd637715020066fdbc79f
muhammed <ekmuhammedwise@gmail.com>
2021-08-11 23:05:50 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 0,
+        speed:2000,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
